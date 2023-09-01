bd1867a137a73b12de54922cc528f52c9bd7410f
Netra05 <108513484+Netra05@users.noreply.github.com>
2023-09-01 09:51:25 +0530

    Add files via upload

---
+++ Logger/src/main/java/com/portal/exercise/Logging.java
@@ -0,0 +1,25 @@
+package com.portal.exercise;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Logging {
+
+	Logger logger=LoggerFactory.getLogger(Logging.class);
+	
+	@GetMapping("/test")
+	public String hello()
+	{
+		logger.trace("A TRACE Message");
+		logger.debug("A DEBUG Message");
+		logger.info("An INFO Messae");
+		logger.warn("A WARN Message");
+		logger.error("An ERROR Message");
+		return "Hello Scaler!";
+	}
+}
